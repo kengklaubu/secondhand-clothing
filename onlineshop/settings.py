7f17e91a393c10aec667326cb31c9750990d5c87
kengkla <kengkla.ar.65@ubu.ac.th>
2025-05-13 00:12:29 +0700

    Fix media file serving for Render

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -4,11 +4,12 @@
 import os
 from pathlib import Path
 
+# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static Files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media Files
 MEDIA_URL = '/media/'
